c1895b0049c9a1f30d2942d3df92e29a8c6f5091
fchomj <fmjcho@gmail.com>
2020-05-26 18:43:51 +0900

    Update chominjoo_200526.R

--- chominjoo_200526.R
+++ chominjoo_200526.R
@@ -11,6 +11,8 @@ library( wordcloud )
 library( wordcloud2 )
 library( RColorBrewer )
 
+library( KoNLP )
+useNIADic()
 library( dplyr )
 library( ggplot2 )
 
@@ -22,3 +24,44 @@ text
 
 
 buildDictionary( ext_dic = 'woorimalsam' )
+pal 
+
+
+
+
+
+
+
+
+
+# 문제 3)
+
+setwd( "C:\\Workspace\\WorkspaceR\\조민주 - wordcloud_problem")
+text5 <- readLines( 'ex_10-5.txt', encoding = 'UTF-8' )
+text5
+
+buildDictionary( ext_dic = 'woorimalsam' )
+pal2 <- brewer.pal( 8, 'Dark2' )
+
+noun5 <- sapply( text5, extractNoun, USE.NAMES = F )
+noun5
+noun6 <- unlist( noun5 )
+
+noun6 <- noun6[ nchar( noun6 ) > 1 ] 
+
+
+wordcount5 <- table( noun6 )
+
+
+sort.noun5 <- sort( wordcount5, decreasing = T )[ 1:10 ]
+sort.noun5
+
+wordcloud( names( wordcount5 ), 
+           freq = wordcount5,
+           main.freq = 3,
+           random.order = F,
+           rot.per = .1,
+           colors = pal2 )
+
+wordcloud2( wordcount5, color = 'random-light',
+            backgroundColor = 'black' )
